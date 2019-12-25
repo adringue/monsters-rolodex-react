9bcdde191227efe62a1a856a2535b268e3ffdb10
adrien <adrien.nguesseu@gmail.com>
2019-12-24 16:49:55 -0800

    Updates

---
+++ precache-manifest.250594d8d9a62c2d139ea96550d2db85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5d9836656560083547b28663e1a0c8",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "4f1c60e0cf42babe6f71",
+    "url": "/monsters-rolodex-react/static/css/main.f3e2eba3.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex-react/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex-react/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f1c60e0cf42babe6f71",
+    "url": "/monsters-rolodex-react/static/js/main.d2eafa3b.chunk.js"
+  },
+  {
+    "revision": "85b14e2a654dc0ff0b5f",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.fcde6bfc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
